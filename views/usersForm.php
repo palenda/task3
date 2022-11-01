9c120e7d01c4dcf1005a36859cb7a152e679a041
Polina <94868256+palenda@users.noreply.github.com>
2022-11-01 14:08:54 +0300

    v1

---
+++ views/usersForm.php
@@ -0,0 +1,26 @@
+<form action="" method="post">
+    <div>
+        <h1>Your form</h1>
+    </div>
+    <div class="mb-3">
+        <label>First Name</label>
+        <input type="text" name = "name" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Last Name</label>
+        <input type="text" name = "surname" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Email</label>
+        <input type="email" name = "email" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Gender</label>
+        <input type="text" name = "gender" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Status</label>
+        <input type="text" name = "status" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>

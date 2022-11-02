9b612e17ec89fe43f288aafcc13d24a6053b4957
Polina <94868256+palenda@users.noreply.github.com>
2022-11-03 01:30:53 +0300

    v2

--- public/index.php
+++ public/index.php
@@ -1,16 +1,13 @@
-<?php
+ <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-use app\controllers\SiteController;
+use app\controllers\UserController;
 use app\core\Application;
 
 $app = new Application(dirname(__DIR__));
 
-$app->router->get('/', function (){
-    return 'Hello World';
-});
-
-$app->router->get('/', [SiteController::class, 'list']);
-$app->router->get('/usersForm', [SiteController::class, 'usersForm']);
-$app->router->post('/usersForm', [SiteController::class, 'usersForm']);
+$app->router->get('/', [UserController::class, 'list']);
+$app->router->get('/usersForm', [UserController::class, 'usersForm']);
+$app->router->post('/usersForm', [UserController::class, 'usersForm']);
 
 $app->run();
+

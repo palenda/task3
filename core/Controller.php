67cdb5a10b053bb879e3a96f703a8a908689bc63
Polina <94868256+palenda@users.noreply.github.com>
2022-11-03 01:26:52 +0300

    v2

--- core/Controller.php
+++ core/Controller.php
@@ -4,8 +4,15 @@ namespace app\core;
 
 class Controller
 {
+    public string $layout = 'main';
+
     public function render($view, $params = [])
     {
         return Application::$app->router->renderView($view, $params);
+    }
+
+    public function setLayout($layout)
+    {
+
     }
 }

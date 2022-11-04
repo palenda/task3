5491ccd3eaa92a9fc9df63ae56c3773e4e6c86c2
Polina <94868256+palenda@users.noreply.github.com>
2022-11-05 01:51:29 +0300

    CR Fixes

--- core/Model.php
+++ core/Model.php
@@ -4,12 +4,12 @@ namespace app\core;
 
 abstract class Model
 {
+    public const RULE_REQUIRED = 'required';
+
     public function loadData($data)
     {
-        foreach ($data as $key => $value)
-        {
-            if(property_exists($this, $key))
-            {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
                 $this->{$key} = $value;
             }
         }

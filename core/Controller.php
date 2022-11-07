30116331f8656ef82f80b555fee849c17e3330bf
Polina <94868256+palenda@users.noreply.github.com>
2022-11-07 14:32:51 +0300

    Fixes 2

--- core/Controller.php
+++ core/Controller.php
@@ -5,10 +5,11 @@ namespace app\core;
 class Controller
 {
     public string $layout = 'main';
+    public View $view;
 
-    public function render($view, $params = [])
+    public function returnView($view, $params = [])
     {
-        return Application::$app->view->renderView($view, $params);
+        return Application::$app->view->render($view, $params);
     }
 
     public function setLayout($layout)

e5e46d26d1f12c0c5ed76a54254dffb67823e89d
Polina <94868256+palenda@users.noreply.github.com>
2022-11-17 18:19:52 +0300

    Updated Router

--- core/Router.php
+++ core/Router.php
@@ -4,45 +4,101 @@ namespace app\core;
 
 class Router
 {
-    public Request $request;
-    public Response $response;
     public View $view;
-    protected array $routes = [];
+    private Request $request;
+    private Response $response;
+    private array $routeMap = [];
 
-    /**
-     * @param Request $request
-     */
     public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
         $this->response = $response;
     }
 
-
-    public function get($path, $callback)
+    public function get(string $url, $callback)
     {
-        $this->routes['get'][$path] = $callback;
+        $this->routeMap['get'][$url] = $callback;
     }
 
-    public function post($path, $callback)
+    public function post(string $url, $callback)
     {
-        $this->routes['post'][$path] = $callback;
+        $this->routeMap['post'][$url] = $callback;
     }
 
-      public function resolve()
-      {
-          $path = $this->request->getPath();
-          $method = $this->request->getMethod();
-          $callback = $this->routes[$method][$path] ?? false;
-          if ($callback === false) {
-              $this->response->setStatusCode(404);
-              return $this->view->render('_404');
-          }
+    /**
+     * @return array
+     */
+    public function getRouteMap($method): array
+    {
+        return $this->routeMap[$method] ?? [];
+    }
 
-          if (is_array($callback)) {
-              Application::$app->controller = new $callback[0]();
-              $callback[0] = new Application::$app->controller();
-          }
-          return call_user_func($callback, $this->request);
-      }
+    public function getCallback()
+    {
+        $method = $this->request->getMethod();
+        $url = $this->request->getUrl();
+        // Trim slashes
+        $url = trim($url, '/');
+
+
+        $routes = $this->getRouteMap($method);
+
+        $routeParams = false;
+
+
+        foreach ($routes as $route => $callback) {
+            // Trim slashes
+            $route = trim($route, '/');
+            $routeNames = [];
+
+            if (!$route) {
+                continue;
+            }
+
+
+            if (preg_match_all('/\{(\w+)(:[^}]+)?}/', $route, $matches)) {
+                $routeNames = $matches[1];
+            }
+
+            $routeRegex = "@^" . preg_replace_callback('/\{\w+(:([^}]+))?}/', fn ($m) => isset($m[2]) ? "({$m[2]})" : '(\w+)', $route) . "$@";
+
+            if (preg_match_all($routeRegex, $url, $valueMatches)) {
+                $values = [];
+                for ($i = 1; $i < count($valueMatches); $i++) {
+                    $values[] = $valueMatches[$i][0];
+                }
+                $routeParams = array_combine($routeNames, $values);
+
+                $this->request->setRouteParams($routeParams);
+                return $callback;
+            }
+        }
+
+        return false;
+    }
+
+    public function resolve()
+    {
+        $method = $this->request->getMethod();
+        $url = $this->request->getUrl();
+        $callback = $this->routeMap[$method][$url] ?? false;
+        if (!$callback) {
+            $callback = $this->getCallback();
+
+            if ($callback === false) {
+                $this->response->setStatusCode(404);
+                return $this->view->render('_404');
+            }
+        }
+
+        if (is_string($callback)) {
+            return $this->view->render($callback);
+        }
+
+        if (is_array($callback)) {
+            Application::$app->controller = new $callback[0]();
+            $callback[0] = new Application::$app->controller();
+        }
+        return call_user_func($callback, $this->request);
+    }
 }

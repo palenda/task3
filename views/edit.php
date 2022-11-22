35522cea5e28fef367789d62318e5556da43a252
Polina <94868256+palenda@users.noreply.github.com>
2022-11-22 18:44:07 +0300

    Updated edit view

--- views/edit.php
+++ views/edit.php
@@ -5,22 +5,36 @@
     <div>
         <h1>Edit user</h1>
     </div>
-    <div class="form-group">
-        <label>First name and last name</label>
-        <input type="text" name = "name" class="form-control" placeholder="Enter your name and surname">
-    </div>
-    <div class="mb-3">
-        <label>Email</label>
-        <input type="email" name = "email" class="form-control" placeholder="Enter your email">
-    </div>
-    <select name="gender">
-        <option selected>Male</option>
-        <option>Female</option>
-    </select><br><br>
-    <select name="status">
-        <option selected>Active</option>
-        <option>Inactive</option>
-    </select><br><br>
-    <button type="submit" class="btn btn-primary" href=".">Update user</button>
+        <?php foreach ($params['user'] as $user): ?>
+        <div class="form-group">
+            <label>First name and last name</label>
+            <input type="hidden" name="id" value="<?= $user['id']; ?>">
+            <input type="text" name = "name" value="<?= $user['name']; ?>" class="form-control" placeholder="Enter your name and surname">
+        </div>
+            <div class="mb-3">
+                <label>Email</label>
+                <input type="email" name = "email" value="<?= $user['email']; ?>" class="form-control" placeholder="Enter your email">
+            </div>
+            <select name="gender">
+                <?php if ($user['gender'] == 'Female'): ?>
+                <option selected><?= $user['gender']; ?></option>
+                <option>Male</option>
+                <?php else :?>
+                <option selected><?= $user['gender']; ?></option>
+                <option>Female</option>
+                <?php endif; ?>
+            </select><br><br>
+            <select name="status">
+                <?php if ($user['status'] == 'Active'): ?>
+                    <option selected><?= $user['status']; ?></option>
+                    <option>Inactive</option>
+                <?php else :?>
+                    <option selected><?= $user['status']; ?></option>
+                    <option>Active</option>
+                <?php endif; ?>
+            </select><br><br>
+            <button type="submit" class="btn btn-primary">Update user</button>
+        <?php endforeach; ?>
 </form>
 </body>
+

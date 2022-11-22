bc63dd2b39ccd5b214aa547c42bdea0b24da2645
Polina <94868256+palenda@users.noreply.github.com>
2022-11-22 18:35:04 +0300

    Updated Routes

--- config/routes.php
+++ config/routes.php
@@ -10,6 +10,7 @@ $app->router->get('/', [AppController::class, 'index']);
 $app->router->get('/users/new', [UserController::class, 'new']);
 $app->router->post('/users/create', [UserController::class, 'create']);
 $app->router->get('/users', [UserController::class, 'show']);
-$app->router->get('/users/edit/{id:\d+}', [UserController::class, 'edit']);
-$app->router->post('/users/{id:\d+}', [UserController::class, 'delete']);
+$app->router->get('/users/edit/{id}', [UserController::class, 'edit']);
+$app->router->get('/users/{id}', [UserController::class, 'delete']);
+$app->router->post('/users/update', [UserController::class, 'update']);
 

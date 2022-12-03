0dc650818ab33f6c98c0b917704f7899cddf5c63
Polina <94868256+palenda@users.noreply.github.com>
2022-12-03 22:10:32 +0300

    Updated UserController

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -2,47 +2,99 @@
 
 namespace app\controllers;
 
-use app\core\Application;
 use app\core\Controller;
 use app\core\Request;
-use app\core\View;
 use app\models\UserModel;
 
 class UserController extends Controller
 {
-    public function list()
+    public function show()
     {
-        $this->setLayout('db');
-        return $this->returnView('list');
+        $userModel = new UserModel();
+        $users = $userModel->index();
+        $count = $userModel->count();
+        $this->params['users'] = $users;
+        $this->params['count'] = $count;
+        $this->params['pages'] = ceil($this->params['count'] / 10);
+        return $this->returnView('users', $this->params);
     }
 
-    public function usersForm(Request $request)
+    public function edit(Request $request)
+    {
+        $userModel = new UserModel();
+        $id = $request->getRouteParam('id');
+        $user = $userModel->edit($id);
+        $this->params['user'] = $user;
+        return $this->returnView('edit', $this->params);
+    }
+
+    public function new()
+    {
+        return $this->returnView('_form');
+    }
+
+    public function create(Request $request)
     {
         $userModel = new UserModel();
         if ($request->isPost()) {
-            $userModel->loadData($request->getBody());
+            if (!empty($_POST) && !empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['gender']) && !empty($_POST['status'])) {
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $gender = $_POST['gender'];
+                $status = $_POST['status'];
 
-//            echo "<pre>";
-//            var_dump($userModel);
-//            echo "</pre>";
-//            exit;
-
-            if ($userModel->validate() && $userModel->pass()) {
-                return 'Success';
+                $userModel->create($name, $email, $gender, $status);
+                $this->params['user'] = $userModel;
             }
-
-            echo "<pre>";
-            var_dump($userModel->errors);
-            echo "</pre>";
-            exit;
-
-            return $this->returnView('usersForm', [
-                'model' => $userModel
-            ]);
         }
-        $this->setLayout('db');
-        return $this->returnView('usersForm', [
-            'model' => $userModel
-        ]);
+
+        return $this->returnView('new', $this->params);
+    }
+
+    public function update(Request $request)
+    {
+        $userModel = new UserModel();
+        if ($request->isPost()) {
+            if (!empty($_POST) && !empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['gender']) && !empty($_POST['status'])) {
+                $id = $_POST['id'];
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $gender = $_POST['gender'];
+                $status = $_POST['status'];
+                $userModel->update($id, $name, $email, $gender, $status);
+                $this->params['user'] = $userModel;
+            }
+        }
+
+        return $this->returnView('update', $this->params);
+    }
+
+    public function deleteChecked(Request $request)
+    {
+        $userModel = new UserModel();
+        if ($request->isPost()) {
+            if (!empty($_POST) && !empty($_POST['multiply'])) {
+                foreach ($_POST['multiply'] as $id) {
+                    $userModel->delete($id);
+                }
+            }
+        }
+        $users = $userModel->index();
+        $count = $userModel->count();
+        $this->params['users'] = $users;
+        $this->params['count'] = $count;
+        return $this->returnView('users', $this->params);
+    }
+
+    public function delete(Request $request)
+    {
+        $id = $request->getRouteParam('id');
+        $userModel = new UserModel();
+        $users = $userModel->index();
+        $count = $userModel->count();
+        $this->params['users'] = $users;
+        $this->params['count'] = $count;
+        $userModel->delete($id);
+        return $this->returnView('users', $this->params);
     }
 }

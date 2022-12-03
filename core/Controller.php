aeba550bab37a749b4f109171cb9af28ae66f3c5
Polina <94868256+palenda@users.noreply.github.com>
2022-12-03 22:14:31 +0300

    Updated Controller Class

--- core/Controller.php
+++ core/Controller.php
@@ -6,6 +6,27 @@ class Controller
 {
     public string $layout = 'main';
     public View $view;
+<?php
+
+namespace app\core;
+
+class Controller
+{
+    public View $view;
+    public Model $model;
+    public array $params = array();
+
+    public function __construct()
+    {
+        $this->model = new Model();
+        $this->view = new View();
+    }
+
+    public function returnView($view, $params = [])
+    {
+        return Application::$app->view->render($view, $params);
+    }
+}
 
     public function returnView($view, $params = [])
     {
